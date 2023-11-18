8ab0e845fd3118b95fb51274c775f301fe89d32b
EmersonPenelli <emersonpenelli@gmail.com>
2023-11-18 16:11:56 -0300

    criação do projeto ts

---
+++ main.js
@@ -0,0 +1,19 @@
+function operacoesBasicas(num1, num2) {
+    // Adição
+    let soma = num1 + num2;
+    console.log(`${num1} + ${num2} = ${soma}`);
+
+    // Subtração
+    let subtracao = num1 - num2;
+    console.log(`${num1} - ${num2} = ${subtracao}`);
+
+    // Multiplicação
+    let multiplicacao = num1 * num2;
+    console.log(`${num1} x ${num2} = ${multiplicacao}`);
+
+    // Divisão
+    let divisao = num1 / num2;
+    console.log(`${num1} / ${num2} = ${divisao}`);
+}
+
+operacoesBasicas(4, 5);
